1a7d80a77b0ebcad9485770207244254fdf65651
Leonardo <leofiore@libero.it>
2012-08-11 17:14:10 +0200

    new general settings file

--- cloudice.py
+++ cloudice.py
@@ -1,11 +1,11 @@
 import soundcloud
+import settings
 import icecast # see https://code.google.com/p/doogradio/ for the original code
 import sys
 import urllib2
 from StringIO import StringIO
+import codecs
 
-client_id = "3d9056dfcd65690dc8d0adff567bc6bd"
-client_secret = "2d311d8a8b11b6b64d12e556476b329c"
 
 # IMPORTANT
 # if no certificates are found, you must provide CURL_CA_BUNDLE env var
@@ -15,7 +15,7 @@ client_secret = "2d311d8a8b11b6b64d12e556476b329c"
 if __name__ == "__main__":
 
     try:
-        client = soundcloud.Client(client_id=client_id)
+        client = soundcloud.Client(client_id=settings.client_id)
         icecast.connect() 
     except Exception, e:
         print e
@@ -25,14 +25,14 @@ if __name__ == "__main__":
     offset = 0
 
     while True:
-        tracks = client.get("/tracks", license="cc-by", tags="rock", types="original", limit=limit, offset=offset)
+        tracks = client.get("/tracks", license="cc-by", tags=settings.tags, order=settings.order, types="original", limit=limit, offset=offset)
 
         for track in tracks:
             print "Now playing: ", track.user["username"], " - ", track.title
-            stream = urllib2.urlopen("%s?client_id=%s" % (track.stream_url, client_id))
+            stream = urllib2.urlopen("%s?client_id=%s" % (track.stream_url, settings.client_id))
             streaming = True
             try:
-                title = str(track.title)
+                title = unicode(track.title.strip(codecs.BOM_UTF8), 'utf-8')
                 icecast.update_metadata(title)
             except:
                 pass
---
+++ settings.py
@@ -0,0 +1,5 @@
+client_id = ""
+client_secret = ""
+
+tags="""rock,pop,post-rock,"indie pop","indie rock",indie"""
+order="hotness" # created_at or hotness
