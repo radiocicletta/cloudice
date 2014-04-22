1334ad1b619ecd3a2d7ed7254bb9d261ca6654b5
leonardo <leofiore@libero.it>
2014-04-22 15:41:42 +0200

    Complete rewrite using libCURL and callbacks Hoping for a more stable stream and a faster download
    Things we learned: using pipes for communication with the subprecess is
    not possible to decouple the chain rawdata->pipein->trans->pipeout->data
    (say, in two separate threads). You need to write, wait, collect, all in
    one. (though is pretty obvious i was hoping to do in a different way)

--- cloudice.py
+++ cloudice.py
@@ -2,17 +2,12 @@ import soundcloud
 import settings
 import icecast  # see https://code.google.com/p/doogradio/ for the original code
 import sys
-import urllib2
+import pycurl
 from urllib import urlencode
 from cStringIO import StringIO
 from time import sleep
 import unicodedata
 import logging
-import re
-try:
-    import simplejson as json
-except:
-    import json
 from subprocess import Popen, PIPE
 from select import select
 
@@ -41,23 +36,71 @@ def SoundCloudGen():
             offset=offset)
         offset = offset + limit
 
+
+proc = None
+
+
+def cbk_write(buf):
+    global proc
+    data = StringIO(buf)
+    data_l = len(buf)
+    while data_l > data.tell():
+        try:
+            proc.stdin.write(data.read(4096))
+            #logger.debug("Data: %d", data.tell())
+        except IOError:
+            logger.error("Restarting transcoder")
+            proc.terminate()
+            proc = Popen(settings.transcoder.split(
+                ' '), stdout=PIPE, stdin=PIPE)
+        except Exception as e:
+            logger.error(e)
+        try:
+            ready = select([proc.stdout], [], [], 0.5)
+            if not len(ready[0]):
+                continue
+            tok = proc.stdout.read(4096)
+            #logger.debug("Transcoded data: %d ", len(tok))
+        except IOError:
+            logger.error("Restarting transcoder")
+            proc.terminate()
+            proc = Popen(settings.transcoder.split(
+                ' '), stdout=PIPE, stdin=PIPE)
+        except Exception as e:
+            logger.error(e)
+        try:
+            icecast.send(tok)
+        except Exception as e:
+            logger.error(e)
+            icecast.close()
+            sleep(1)
+            icecast.connect()
+            logger.error("Failed connection with icecast server")
+
+
 if __name__ == "__main__":
 
     try:
         gen = SoundCloudGen()
         logger.info("Connecting to icecast")
-        icecast.connect()
     except Exception as e:
         logger.error(e)
         sys.exit(1)
 
+    errorcount = 0.0
+    playcount = 10.0
+    curl = pycurl.Curl()
+    curl.setopt(pycurl.FOLLOWLOCATION, 1)
+    curl.setopt(pycurl.MAXREDIRS, 5)
+    curl.setopt(pycurl.CONNECTTIMEOUT, 30)
+    curl.setopt(pycurl.TIMEOUT, 300)
+    curl.setopt(pycurl.NOSIGNAL, 1)
+    curl.setopt(pycurl.WRITEFUNCTION, cbk_write)
+
     if len(settings.transcoder.strip()):
         proc = Popen(settings.transcoder.split(' '), stdout=PIPE, stdin=PIPE)
     else:
-        proc = None
-
-    errorcount = 0.0
-    playcount = 10.0
+        sys.exit(1)
 
     while errorcount / playcount <= 1:
         try:
@@ -71,20 +114,11 @@ if __name__ == "__main__":
 
         playcount = playcount + 1
 
-        for track in tracks:
-            try:
-                logger.debug(track.stream_url)
-                stream = urllib2.urlopen("%s?client_id=%s" % (
-                    track.stream_url, settings.client_id), timeout=10)
-                streaming = True
-            except Exception as e:
-                logger.error("urllib2.urlopen: %s" % e)
-                logger.error("[ErrorRatio] %d %d %f" % (
-                    errorcount, playcount, errorcount / playcount))
-                errorcount = errorcount + 1
-                if errorcount / playcount >= 1:
-                    sys.exit(1)
+        icecast.connect()
 
+        for track in tracks:
+            curl.setopt(pycurl.URL, "%s?client_id=%s" % (
+                track.stream_url, settings.client_id))
             try:
                 username = track.user["username"]
                 title = track.title
@@ -95,52 +129,6 @@ if __name__ == "__main__":
                 logger.error(e)
                 logger.error("[ErrorRatio] %d %d %f" % (
                     errorcount, playcount, errorcount / playcount))
-            readcount = 0
-            while streaming:
-                io = StringIO()
-                try:
-                    if proc:
-                        proc.stdin.write(stream.read(4096))
-                        ready = select([proc.stdout], [], [], 1)
-                        if not len(ready[0]):
-                            readcount = readcount + 1
-                            if readcount == 10:
-                                streaming = False
-                            continue
-                        tok = proc.stdout.read(4096)
-                        readcount = 0
-                        if len(tok) < 4096:
-                            streaming = False
-                            continue
-                        io.write(tok)
-                    else:
-                        io.write(stream.read())
-                except IOError:
-                    logger.debug("IOError")
-                    proc.terminate()
-                    proc = Popen(settings.transcoder.split(
-                        ' '), stdout=PIPE, stdin=PIPE)
-                    logger.debug("reopened")
-                except Exception as e:
-                    logger.error(e)
-                    errorcount = errorcount + 1
-                    streaming = False
-                    continue
-                try:
-                    icecast.send(io.getvalue())
-                except:
-                    icecast.close()
-                    sleep(1)
-                    icecast.connect()
-                    logger.error("Failed connection with icecast server")
-                    logger.error("[ErrorRatio] %d %d %f" % (
-                        errorcount, playcount, errorcount / playcount))
-                    errorcount = errorcount + 1
-                    streaming = False
-                finally:
-                    if not io.tell():
-                        streaming = False
-                if errorcount / playcount >= 1:
-                    sys.exit(1)
+            curl.perform()
 
     icecast.close()
